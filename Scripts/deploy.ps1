b4387ed38abb6a70656d0cd61a730df7c7f36ca1
Thiago Di Giorgio <thiago.digiorgio@version1.com>
2019-05-22 16:55:52 +0100

    Edited master template

---
+++ Scripts/deploy.ps1
@@ -0,0 +1,10 @@
+Login-AzAccount
+
+Select-AzSubscription -Subscription "UK D&C Solutions"
+
+Get-AzMarketplaceTerms -Publisher fortinet -Product fortinet_fortigate-vm_v5 -name fortinet_fg-vm_payg | Set-AzMarketplaceTerms -Accept
+
+
+Test-AzDeployment  -Location "uksouth" -TemplateFile ".\master-template.json" -Verbose 
+
+New-AzDeployment  -Location "uksouth" -TemplateFile ".\master-template.json" -Verbose 
